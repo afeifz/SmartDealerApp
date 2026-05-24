02b4fcbf7a8185d0201d81b692a714d8ed206628
Mohamed <mohamedafifsmaili@gmail.com>
2026-05-24 17:48:09 -0300

    Feat: Auth API

--- src/api/api.ts
+++ src/api/api.ts
@@ -5,9 +5,12 @@ import axios from 'axios';
 const api = axios.create({
 
   baseURL: 'http://163.176.204.45:8080/api/v1/',
-
   timeout: 30000,
 
- 
+   auth: {
+    username: 'admin.ti',
+    password: 'admin123'
+
+  }
 
 });
